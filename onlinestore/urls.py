d98b01101cd8daf6307a4a5ac1aa8b2215bfefde
Nick Lofaro <5376991+nickoralias@users.noreply.github.com>
2020-04-16 21:20:16 -0700

    create api

--- onlinestore/urls.py
+++ onlinestore/urls.py
@@ -20,7 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('products.urls')),
+    path('api/', include('products.urls')),
 ]
 
 if settings.DEBUG:
